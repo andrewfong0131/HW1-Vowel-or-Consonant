f418499a66b7c0b8a080a49e8d08e1ed1ea4f58d
andrewfong0131 <57347094+andrewfong0131@users.noreply.github.com>
2019-11-11 23:29:18 +0800

    Add files via upload

---
+++ charvowelconsonant.py
@@ -0,0 +1,7 @@
+letter = input("Enter a character: ")
+
+if(letter == 'A' or letter == 'a' or letter == 'E' or letter == 'e' or letter == 'I' or letter == 'i'
+       or letter == 'O' or letter == 'o' or letter == 'U' or letter == 'u'):
+    print("The character is a vowel.")
+else:
+    print("The character is a consonant.")
